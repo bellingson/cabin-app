0d1a9de8426ae687fd596016f6d6f1d881e27cd5
bellingson <ben@nofluffjuststuff.com>
2018-02-27 10:40:38 -0500

    updating try your best

--- client/app/testing/face-test/try-your-best.component.ts
+++ client/app/testing/face-test/try-your-best.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { Router } from '@angular/router';
 
+const WORD_DELAY = 200;
+
 @Component({
   selector: 'app-try-your-best',
   templateUrl: './try-your-best.component.html',
@@ -9,10 +11,47 @@ import { Router } from '@angular/router';
 })
 export class TryYourBestComponent implements OnInit {
 
+  msg = 'Please try your best to concentrate on the task. Your performance may be compared anonymously with other participants\' performance at a later time.';
+
+  words: Array<string>;
+  currentIndex = 0;
+
+  displayMessage: string = '';
+  messageComplete = false;
+
   constructor(private router: Router) { }
 
   ngOnInit() {
+
+    this.words = this.msg.split(' ');
+
+    // console.log(this.words);
+
+    this.showNextWord();
+
   }
+
+  showNextWord() {
+
+    const word = this.words[this.currentIndex++];
+
+    this.displayMessage += word + ' ';
+
+    if(this.currentIndex < this.words.length) {
+      this.setMyTimeout(this.showNextWord.bind(this),WORD_DELAY);
+    } else {
+      this.setMyTimeout(() => {
+        this.messageComplete = true;
+      }, 3000);
+
+    }
+
+  }
+
+  setMyTimeout(fn: any, time: number) {
+    setTimeout(fn, time);
+  }
+
 
   next() {
     this.router.navigateByUrl('/t/focus');
